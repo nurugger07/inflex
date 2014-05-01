fbae92fd62dfb3ca6134f1d77b569d9fca260f3d
Hashrocket Workstation <dev@hashrocket.com>
2014-05-01 00:11:08 -0400

    Add Hex check & start

--- mix.exs
+++ mix.exs
@@ -1,11 +1,24 @@
+Code.ensure_loaded?(Hex) and Hex.start
+
 defmodule Inflex.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :inflex,
+    [ 
+      app: :inflex,
       version: "0.2.0",
       elixir: ">= 0.13.0",
-      deps: [] ]
+      deps: [],
+      package: [ 
+        files: ["lib", "mix.exs", "README*", "LICENSE*"],
+        contributors: ["Johnny Winn"],
+        licenses: ["Apache 2.0"],
+        links: [ github: "https://github.com/nurugger07/inflex" ] 
+      ],
+      description: """
+      An Elixir library for handling word inflections. 
+      """,
+    ]
   end
 
   # Configuration for the OTP application
