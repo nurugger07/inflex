c04c11134c095006239c9c663ab56ed9b562fbe7
Johnny Winn <j.winn.v@gmail.com>
2016-10-20 21:35:15 +0200

    Bump version for 1.8 release

--- mix.exs
+++ mix.exs
@@ -6,14 +6,15 @@ defmodule Inflex.Mixfile do
   def project do
     [
       app: :inflex,
-      version: "1.7.0",
+      version: "1.8.0",
       elixir: ">= 1.0.0",
-      deps: [],
+      deps: deps,
       package: [
         files: ["lib", "mix.exs", "README*", "LICENSE*"],
         contributors: ["Johnny Winn"],
         licenses: ["Apache 2.0"],
-        links: %{ "GitHub" => "https://github.com/nurugger07/inflex" }
+        links: %{ "GitHub" => "https://github.com/nurugger07/inflex" },
+        maintainers: ["Johnny Winn"]
       ],
       description: """
       An Elixir library for handling word inflections.
@@ -24,5 +25,9 @@ defmodule Inflex.Mixfile do
   # Configuration for the OTP application
   def application do
     []
+  end
+
+  def deps do
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
