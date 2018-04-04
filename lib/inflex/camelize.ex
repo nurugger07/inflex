1adbc88845ea2771cf514661ba7e3bbca3f590c9
Johannes Baum <baum@bw-webgestaltung.de>
2018-04-04 03:35:06 +0200

    Fix camelize lower (#66)

--- lib/inflex/camelize.ex
+++ lib/inflex/camelize.ex
@@ -3,8 +3,9 @@ defmodule Inflex.Camelize do
 
   def camelize(word, option\\:upper) do
     case Regex.split(~r/(?:^|[-_])|(?=[A-Z])/, to_string(word)) do
-      words -> words |> camelize_list(option)
-                     |> Enum.join
+      words ->
+        words |> Enum.filter(&(&1 != "")) |> camelize_list(option)
+        |> Enum.join()
     end
   end
 
