b30e9358ef690ca255fa8b33867ad00ea0af7844
flowerett <nick.chernyshev@gmail.com>
2018-09-27 13:48:22 +0200

    Add mix formatter

--- lib/inflex/parameterize.ex
+++ lib/inflex/parameterize.ex
@@ -1,9 +1,9 @@
 defmodule Inflex.Parameterize do
   @moduledoc false
 
-  def parameterize(word, option\\"-") do
-    Regex.split(~r/\s|\%20/, word) |>
-    Enum.join(option) |>
-    String.downcase
+  def parameterize(word, option \\ "-") do
+    Regex.split(~r/\s|\%20/, word)
+    |> Enum.join(option)
+    |> String.downcase()
   end
 end
